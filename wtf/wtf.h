7a5f42c3bb415b5c54c91563c2c94ebfcb208321
Keegan van der Laag <kvanderlaag@gmail.com>
2018-07-05 16:05:17 -0700

    Match style in contains_shit
    Modifies contains_shit to match the style of doesnt_contain_shit.

--- wtf/wtf.h
+++ wtf/wtf.h
@@ -3,8 +3,8 @@
 #include <stdexcept>
 
 #define sucks ==nullptr
-#define does_not_suck !=nullptr
-#define contains_shit(X) !(X).empty()
+#define does_not_suck != nullptr
+#define contains_shit size > 0
 #define doesnt_contain_shit empty()
 #define then_fuck_it exit(1);
 #define fuck_this_i_am_out return 0
